2aae6aead82fc61bcd464adaabffb768e96ab9fb
Charlie Jackson <charlie@charliejackson.com>
2016-08-24 17:01:04 +0100

    Temporarily disabled hidden admin menus

--- wp-content/themes/indiajackson-0-0-1/functions.php
+++ wp-content/themes/indiajackson-0-0-1/functions.php
@@ -29,85 +29,84 @@ ADD THEME SUPPORT
 REMOVE REDUNDANT FEATURES
 ----------------------------- */	
 	// Disable support for comments and trackbacks in post types
-	function df_disable_comments_post_types_support() {
-		$post_types = get_post_types();
-		foreach ($post_types as $post_type) {
-			if(post_type_supports($post_type, 'comments')) {
-				remove_post_type_support($post_type, 'comments');
-				remove_post_type_support($post_type, 'trackbacks');
-			}
-		}
-	}
-	add_action('admin_init', 'df_disable_comments_post_types_support');
+	// function df_disable_comments_post_types_support() {
+	// 	$post_types = get_post_types();
+	// 	foreach ($post_types as $post_type) {
+	// 		if(post_type_supports($post_type, 'comments')) {
+	// 			remove_post_type_support($post_type, 'comments');
+	// 			remove_post_type_support($post_type, 'trackbacks');
+	// 		}
+	// 	}
+	// }
+	// add_action('admin_init', 'df_disable_comments_post_types_support');
 	
-	// Close comments on the front-end
-	function df_disable_comments_status() {
-		return false;
-	}
-	add_filter('comments_open', 'df_disable_comments_status', 20, 2);
-	add_filter('pings_open', 'df_disable_comments_status', 20, 2);
+	// // Close comments on the front-end
+	// function df_disable_comments_status() {
+	// 	return false;
+	// }
+	// add_filter('comments_open', 'df_disable_comments_status', 20, 2);
+	// add_filter('pings_open', 'df_disable_comments_status', 20, 2);
 	
-	// Hide existing comments
-	function df_disable_comments_hide_existing_comments($comments) {
-		$comments = array();
-		return $comments;
-	}
-	add_filter('comments_array', 'df_disable_comments_hide_existing_comments', 10, 2);
+	// // Hide existing comments
+	// function df_disable_comments_hide_existing_comments($comments) {
+	// 	$comments = array();
+	// 	return $comments;
+	// }
+	// add_filter('comments_array', 'df_disable_comments_hide_existing_comments', 10, 2);
 	
-	// Remove comments page in menu
-	function df_disable_comments_admin_menu() {
-		remove_menu_page('edit-comments.php');
-		remove_menu_page('edit.php?post_type=page');
-		remove_menu_page('edit.php');
-		remove_menu_page('upload.php');
-		remove_menu_page( 'index.php' );
-		remove_menu_page( 'tools.php' );
-		remove_menu_page( 'users.php' );
-		remove_menu_page( 'plugins.php' );
-		remove_menu_page( 'themes.php' );
-		remove_menu_page( 'options-general.php' );
-		remove_submenu_page( 'options-general.php', 'options-writing.php' );
-		remove_submenu_page( 'options-general.php', 'options-reading.php' );
-		remove_submenu_page( 'options-general.php', 'options-discussion.php' );
-		remove_submenu_page( 'options-general.php', 'options-media.php' );
-		remove_submenu_page( 'options-general.php', 'options-permalink.php' );
-		remove_submenu_page( 'options-general.php', 'options-permalink.php' );
-	}
-	add_action('admin_menu', 'df_disable_comments_admin_menu');
+	// // Remove comments page in menu
+	// function df_disable_comments_admin_menu() {
+	// 	remove_menu_page('edit-comments.php');
+	// 	remove_menu_page('edit.php?post_type=page');
+	// 	remove_menu_page('edit.php');
+	// 	remove_menu_page('upload.php');
+	// 	remove_menu_page( 'index.php' );
+	// 	remove_menu_page( 'tools.php' );
+	// 	remove_menu_page( 'users.php' );
+	// 	remove_menu_page( 'plugins.php' );
+	// 	remove_menu_page( 'themes.php' );
+	// 	remove_menu_page( 'options-general.php' );
+	// 	remove_submenu_page( 'options-general.php', 'options-writing.php' );
+	// 	remove_submenu_page( 'options-general.php', 'options-reading.php' );
+	// 	remove_submenu_page( 'options-general.php', 'options-discussion.php' );
+	// 	remove_submenu_page( 'options-general.php', 'options-media.php' );
+	// 	remove_submenu_page( 'options-general.php', 'options-permalink.php' );
+	// 	remove_submenu_page( 'options-general.php', 'options-permalink.php' );
+	// }
+	// add_action('admin_menu', 'df_disable_comments_admin_menu');
 	
-	// Redirect any user trying to access comments page
-	function df_disable_comments_admin_menu_redirect() {
-		global $pagenow;
-		if ($pagenow === 'edit-comments.php') {
-			wp_redirect(admin_url()); exit;
-		}
-	}
-	add_action('admin_init', 'df_disable_comments_admin_menu_redirect');
+	// // Redirect any user trying to access comments page
+	// function df_disable_comments_admin_menu_redirect() {
+	// 	global $pagenow;
+	// 	if ($pagenow === 'edit-comments.php') {
+	// 		wp_redirect(admin_url()); exit;
+	// 	}
+	// }
+	// add_action('admin_init', 'df_disable_comments_admin_menu_redirect');
 	
-	// Remove comments metabox from dashboard
-	function df_disable_comments_dashboard() {
-		remove_meta_box('dashboard_recent_comments', 'dashboard', 'normal');
-	}
-	add_action('admin_init', 'df_disable_comments_dashboard');
+	// // Remove comments metabox from dashboard
+	// function df_disable_comments_dashboard() {
+	// 	remove_meta_box('dashboard_recent_comments', 'dashboard', 'normal');
+	// }
+	// add_action('admin_init', 'df_disable_comments_dashboard');
 	
-	// Remove comments links from admin bar
-	function df_disable_comments_admin_bar() {
-		if (is_admin_bar_showing()) {
-			remove_action('admin_bar_menu', 'wp_admin_bar_comments_menu', 60);
-		}
-	}
-	add_action('init', 'df_disable_comments_admin_bar');
+	// // Remove comments links from admin bar
+	// function df_disable_comments_admin_bar() {
+	// 	if (is_admin_bar_showing()) {
+	// 		remove_action('admin_bar_menu', 'wp_admin_bar_comments_menu', 60);
+	// 	}
+	// }
+	// add_action('init', 'df_disable_comments_admin_bar');
 	
 	
-	add_action( 'admin_menu', 'register_my_custom_menu_page' );
+	// add_action( 'admin_menu', 'register_my_custom_menu_page' );
 
-	function register_my_custom_menu_page() {
+	// function register_my_custom_menu_page() {
 	
-		add_menu_page( 'Nav Bar', 'Nav Bar', 'manage_options', 'nav-menus.php' );
-		add_menu_page( 'Settings', 'Settings', 'manage_options', 'options-general.php' );
-		add_menu_page( 'Your Profile', 'Your Profile', 'manage_options', 'profile.php' );
-	
-	}
+	// 	add_menu_page( 'Nav Bar', 'Nav Bar', 'manage_options', 'nav-menus.php' );
+	// 	add_menu_page( 'Settings', 'Settings', 'manage_options', 'options-general.php' );
+	// 	add_menu_page( 'Your Profile', 'Your Profile', 'manage_options', 'profile.php' );
+	// }
 
 /* -----------------------------
 ENQUE STYLES AND SCRIPTS
