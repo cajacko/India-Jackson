e6035197793ca882f3a25e8feb1ede96d6579ee1
Charlie Jackson <contact@charliejackson.com>
2017-07-12 17:51:40 +0100

    Added static markup for single project view

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,35 +1,47 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import ReactMarkdown from 'react-markdown';
-import dateString from 'helpers/dateString';
+// import PropTypes from 'prop-types';
+import Item from 'containers/Item/Item';
+import Image from 'components/Image/Image';
 
-const Project = ({ fields }) => {
-  const date = dateString(fields.displayDate['en-GB']);
+const Project = ({ fields }) => (
+  <section>
+    <header>
+      <h1>{fields.title['en-GB']}</h1>
+      { fields.description && <p>{fields.description['en-GB']}</p> }
+      { fields.backgroundImage && <Item asset element={Image} itemId={fields.backgroundImage['en-GB'].sys.id} /> }
+    </header>
+    <ul>
+      {
+        fields.images['en-GB'].map(({ sys }) => (
+          <li key={sys.id}>
+            <Item
+              element={Image}
+              itemId={sys.id}
+              asset
+            />
+          </li>
+        ))
+      }
+    </ul>
+  </section>
+);
 
-  return (
-    <article className="Project">
-      <h2 className="Project-title">{fields.title['en-GB']}</h2>
-      <div className="Project-meta">
-        <p className="Project-date">Last Updated: {date}</p>
-      </div>
-
-      <ReactMarkdown source={fields.content['en-GB']} />
-    </article>
-  );
-};
-
-Project.propTypes = {
-  fields: PropTypes.shape({
-    displayDate: PropTypes.shape({
-      'en-GB': PropTypes.string,
-    }),
-    title: PropTypes.shape({
-      'en-GB': PropTypes.string,
-    }),
-    content: PropTypes.shape({
-      'en-GB': PropTypes.string,
-    }),
-  }).isRequired,
-};
+// SiteHeader.propTypes = {
+//   fields: PropTypes.shape({
+//     sitetitle: PropTypes.shape({
+//       'en-GB': PropTypes.string,
+//     }),
+//     description: PropTypes.shape({
+//       'en-GB': PropTypes.string,
+//     }),
+//     image: PropTypes.shape({
+//       'en-GB': PropTypes.shape({
+//         sys: PropTypes.shape({
+//           id: PropTypes.string,
+//         }),
+//       }),
+//     }),
+//   }).isRequired,
+// };
 
 export default Project;
--- src/constants/typeComponentMap.js
+++ src/constants/typeComponentMap.js
@@ -2,10 +2,12 @@ import FourOhFour from 'components/FourOhFour/FourOhFour';
 import SiteNav from 'components/SiteNav/SiteNav';
 import ProfileBanner from 'components/ProfileBanner/ProfileBanner';
 import ProjectGrid from 'components/ProjectGrid/ProjectGrid';
+import Project from 'containers/Project/Project';
 
 export default {
   siteNav: SiteNav,
   fourOhFourComponent: FourOhFour,
   profileBanner: ProfileBanner,
   projectGrid: ProjectGrid,
+  singleProjectComponent: Project,
 };
